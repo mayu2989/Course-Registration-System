01b398e31b430d17de467254199c35d95c04a5ab
Mayuresh Itankar <flame191835@gmail.com>
2026-03-10 11:55:19 +0530

    Fix formatting in SecurityConfig for API docs path

--- src/main/java/com/mayu298/courseregistrationsystem/config/SecurityConfig.java
+++ src/main/java/com/mayu298/courseregistrationsystem/config/SecurityConfig.java
@@ -148,7 +148,7 @@ public class SecurityConfig {
                                 "/auth/login",
                                 "/swagger-ui/",
                                 "/swagger-ui.html",
-        "                        /v3/api-docs/"
+                                "/v3/api-docs/"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
